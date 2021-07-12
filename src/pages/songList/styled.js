d9115c7b34b34140036fa0269c4e85017e6f2012
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-12 17:52:32 -0300

    Song list page and style

---
+++ src/pages/songList/styled.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 15%;
+  justify-content: center;
+  width: 60vw;
+  @media only screen and (max-width: 400px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 88vw;
+  }
+`;
+
+export const AddSongButton = styled.button`
+  order: 2;
+  background-color: #3d9690;
+  color: #ffffff;
+  border-radius: 5px;
+  border: none;
+  height: 92%;
+  align-self: center;
+  margin-left: 10px;
+  grid-row: 1 / 3;
+  grid-column: 2 / 3;
+  font-size: 20px;
+  cursor: pointer;
+  :hover {
+    background-color: #43bab8;
+    color: #ffffff;
+    transition: all 0.5s ease 0s;
+  }
+  @media only screen and (max-width: 400px) {
+    margin: 0 auto;
+    margin-bottom: 15px;
+    margin-top: 5px;
+    padding: 8px 5px;
+    width: 88vw;
+  }
+`;
+
+export const FormTextContainer = styled.div`
+  order: 3;
+  @media only screen and (max-width: 400px) {
+    display: flex;
+    order: 2;
+  }
+`;
