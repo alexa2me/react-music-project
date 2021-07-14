0054276e7d7c9383dea1a3c29fa6a41d23cdf7cc
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-14 13:45:27 -0300

    Services de login e sign up finalizados

--- src/services/user.js
+++ src/services/user.js
@@ -2,34 +2,34 @@ import BASE_URL from "../constants/urls";
 import axios from "axios";
 import { goToSongsList } from "../routes/coordinator";
 
-export const login = (body, clear, history, setAccessButton, setIsLoading) => {
+export const login = (body, clear, history, setIsLoading) => {
   setIsLoading(true);
   axios
-    .post(`${BASE_URL}/login`, body)
+    .post(`${BASE_URL}/users/login`, body)
     .then((res) => {
       localStorage.setItem("token", res.data.token);
       clear();
       setIsLoading(false);
       goToSongsList(history);
-      setAccessButton("Logout");
+      // setAccessButton("Logout");
     })
     .catch((err) => {
       setIsLoading(false);
-      alert(err.response.data.message);
+      console.log(err.response.data.message);
     });
 };
 
-export const signup = (body, clear, history, setAccessButton, setIsLoading) => {
+export const signup = (body, clear, history, setIsLoading) => {
   setIsLoading(true);
   axios
-    .post(`${BASE_URL}/signup`, body)
+    .post(`${BASE_URL}/users/signup`, body)
     .then((res) => {
       localStorage.setItem("token", res.data.token);
       alert("User created successfully!");
       clear();
       setIsLoading(false);
       goToSongsList(history);
-      setAccessButton("Logout");
+      // setAccessButton("Logout");
     })
     .catch((err) => {
       setIsLoading(false);
