a291012d3551d0938936e6d526371e150fce6654
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2021-07-21 14:16:56 -0300

    Rotas e coordenadas refatoradas.

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "../pages/login/Login";
-import SongList from "../pages/songList/SongList";
+import HomePage from "../pages/homePage/HomePage";
 import SignUp from "../pages/signUp/SignUp";
+import CreateSong from "../pages/createSong/CreateSong";
+import { BrowserRouter } from "react-router-dom";
 
-const Router = ({ accessButton, setAccessButton }) => {
+const Router = () => {
   return (
-    <Switch>
-      <Route exact path="/login">
-        <Login accessButton={accessButton} setAccessButton={setAccessButton} />
-      </Route>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/users/login">
+          <Login />
+        </Route>
 
-      <Route exact path="/signup">
-        <SignUp accessButton={accessButton} setAccessButton={setAccessButton} />
-      </Route>
+        <Route exact path="/users/signup">
+          <SignUp />
+        </Route>
 
-      <Route exact path="/">
-        <SongList />
-      </Route>
-    </Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+
+        <Route exact path="/createsong">
+          <CreateSong />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
